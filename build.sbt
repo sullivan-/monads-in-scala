9b01f65d8ba1d36f643ac3577b17189ec7fa8291
sullivan- <sullymandias@gmail.com>
2013-08-08 08:42:27 -0400

    wip in Lisst post

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ name := "scala-monads"
 
 version := "0.0"
 
-scalaVersion := "2.10.1-RC1"
+scalaVersion := "2.10.2"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "2.0.M5b" % "test"
 
---
+++ src/main/scala/lisst/Lisst.scala
@@ -0,0 +1,52 @@
+package lisst
+
+import scala.annotation.tailrec
+
+object Lisst {
+ 
+  def apply[A](elems: A*): Lisst[A] = reverseSeq(elems.reverse)
+
+  private def reverseSeq[A](elems: Seq[A]): Lisst[A] = {
+    var result: Lisst[A] = EmptyLisst
+    elems.foreach { elem => result = NonEmptyLisst(elem, result) }
+    result    
+  }
+
+}
+ 
+sealed trait Lisst[+A] {
+
+  def flatMap[B](f: A => Lisst[B]): Lisst[B] = {
+    var reverseBs = List[B]()
+    this.foreach { a =>
+      f(a).foreach { b =>
+        reverseBs = b +: reverseBs
+      }
+    }
+    Lisst.reverseSeq(reverseBs)
+  }
+
+  /** Walks through the lisst, applying function f to every element in turn. */
+  @tailrec
+  final def foreach(f: A => Unit): Unit = this match {
+    case NonEmptyLisst(head, tail) => {
+      f(head)
+      tail.foreach(f)
+    }
+    case EmptyLisst =>
+  }
+
+  def map[B](f: A => B): Lisst[B] = flatMap { a => Lisst(f(a)) }
+
+  def flatten[B](implicit asLisstLisst: Lisst[A] <:< Lisst[Lisst[B]]): Lisst[B] = 
+    asLisstLisst(this) flatMap identity
+
+}
+
+private case class NonEmptyLisst[+A](
+  head: A,
+  tail: Lisst[A])
+extends Lisst[A]
+
+private case object EmptyLisst
+extends Lisst[Nothing]
